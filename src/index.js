41ee9fdbbd24aca3c9beed06badc2f49d7b9ec60
Mihail Dorozhko <dominick.alone@gmail.com>
2021-08-14 17:22:07 +0300

    feat: add half solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let openedBrackets = bracketsConfig.map(function(item) {
+    return item[0];
+  });
+
+  let closedBrackets = bracketsConfig.map(function(item) {
+    return item[1];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+
+    if (openedBrackets.includes(symbol)) {
+      stack.push(symbol);
+    } else if (stack.length === 0) {
+      return false;
+    }
+
+  if (closedBrackets.includes(symbol)) {
+      stack.pop();
+    } else if (stack.length === 0) {
+      return false;
+    }
+
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
